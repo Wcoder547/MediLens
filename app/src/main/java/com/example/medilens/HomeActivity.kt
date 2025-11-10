1ba26072f43ab17dbd6f27da3b62f7fccd4d820f
wcoder547 <mw5667155@gmail.com>
2025-11-10 18:34:58 +0500

    Home tab/page done

--- app/src/main/java/com/example/medilens/HomeActivity.kt
+++ app/src/main/java/com/example/medilens/HomeActivity.kt
@@ -1,8 +1,11 @@
 package com.example.medilens
 
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.core.content.ContextCompat
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +14,20 @@ class HomeActivity : ComponentActivity() {
 
         val userName = intent.getStringExtra("USER_NAME") ?: "User"
         val userEmail = intent.getStringExtra("USER_EMAIL") ?: ""
+
+        val textView = findViewById<TextView>(R.id.tvGreeting)
+        val paint = textView.paint
+        val width = paint.measureText(textView.text.toString())
+        val textShader = LinearGradient(
+            0f, 0f, width, textView.textSize,
+            intArrayOf(
+                ContextCompat.getColor(this, R.color.primary),
+                ContextCompat.getColor(this, R.color.secondary)
+            ),
+            null,
+            Shader.TileMode.CLAMP
+        )
+        textView.paint.shader = textShader
 
 //        findViewById<TextView>(R.id.welcomeText).text =
 //            "Welcome, $userName\nEmail: $userEmail"
